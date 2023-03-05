5af2945d6a0385bb600d83a02051bb3043c4c4eb
Yogesh saini <84733974+yogeshsaini1997@users.noreply.github.com>
2023-03-05 22:39:33 +0530

    stashed changes

--- Backend_TODOAPP/Mongo_Connection/MongoDbConnect.js
+++ Backend_TODOAPP/Mongo_Connection/MongoDbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoDbUri = "mongodb+srv://yogesh:eKzSwxRF2YLHqZnk@cluster0.2yuxbb3.mongodb.net/TodoApp";
+const mongoDbUri = process.env.MONGO_URI
 
 console.log("hiii")
 
